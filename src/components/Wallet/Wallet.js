36a08972257b83710ecc4e142ffc9c65c44ae222
Nikolay Chankov <nikolay.chankov@gostartups.net>
2022-11-30 13:55:17 +0200

    image compression

--- src/components/Wallet/Wallet.js
+++ src/components/Wallet/Wallet.js
@@ -20,32 +20,31 @@ export default function Wallet() {
                 <div className="padding-global">
                     <div className="container-large">
                         <div className="main-container">
-                            <div className="max-width-full waller-slider  margin-slider w-tab-content">
-                                <WalletSlider onClickNav={onClickNav} responsiveSliderData={responsiveSliderData} sliderData={slideImages}/>
-                            </div>
-                            <div data-current="Tabs"  data-easing="ease-out" data-duration-in="300"
-                                 data-duration-out="100" className="tabs tabs-wealth margin-top margin-huge w-tabs">
-                                <div className="max-width-small-slider">
-                                    <AnimationOnScroll  animateIn="animate__fadeInUp" className="wallet_wrapper-right animate__animated animate__fadeInUp">
-                                        <div className="wallet-content">
-                                            <div className="label is-lime slide-from-bottom">{scrollImages[itemIndex].h1}</div>
-                                            <div style={{margin: "0px 0 10px auto"}}>
-                                            </div>
-                                        </div>
-
-                                        <h2 className="heading-style-h2 wallet-header margin-bottom margin-large slide-from-bottom">
-                                            {scrollImages[itemIndex].caption}
-                                        </h2>
-                                        <p className="text-color-grey wallet-text text-size-large slide-from-bottom">{scrollImages[itemIndex].text}
-                                        </p>
-                                        <a target="_blank"  rel="noreferrer" href={scrollImages[itemIndex].url}
-                                           className="button btn-wallet is-large with-icon width-260 w-inline-block">
-                                            {scrollImages[itemIndex].urlText}
-                                        </a>
-                                    </AnimationOnScroll>
+                                <div className="max-width-full waller-slider  margin-slider w-tab-content">
+                                    <WalletSlider onClickNav={onClickNav} responsiveSliderData={responsiveSliderData} sliderData={slideImages}/>
                                 </div>
-                            </div>
+                                <div data-current="Tabs"  data-easing="ease-out" data-duration-in="300"
+                                     data-duration-out="100" className="tabs tabs-wealth margin-top margin-huge w-tabs">
+                                    <div className="max-width-small-slider">
+                                        <AnimationOnScroll  animateIn="animate__fadeInUp" className="wallet_wrapper-right animate__animated animate__fadeInUp">
+                                            <div className="wallet-content">
+                                                <div className="label is-lime slide-from-bottom">{scrollImages[itemIndex].h1}</div>
+                                                <div style={{margin: "0px 0 10px auto"}}>
+                                                </div>
+                                            </div>
 
+                                            <h2 className="heading-style-h2 wallet-header margin-bottom margin-large slide-from-bottom">
+                                                {scrollImages[itemIndex].caption}
+                                            </h2>
+                                            <p className="text-color-grey wallet-text text-size-large slide-from-bottom">{scrollImages[itemIndex].text}
+                                            </p>
+                                            <a target="_blank"  rel="noreferrer" href={scrollImages[itemIndex].url}
+                                               className="button btn-wallet is-large with-icon width-260 w-inline-block">
+                                                {scrollImages[itemIndex].urlText}
+                                            </a>
+                                        </AnimationOnScroll>
+                                    </div>
+                            </div>
                         </div>
                     </div>
                 </div>
