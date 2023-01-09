82be26573036af4d516409a850c8e27b5e227f4a
Nikolay Chankov <nikolay.chankov@gostartups.net>
2023-01-09 14:42:17 +0200

    Roadmap component done

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -1,54 +1,64 @@
 import * as React from "react";
-import { Timeline } from 'antd';
+import { Timeline } from "antd";
 
-export default function Roadmap({ background,  roadmapData }) {
+export default function Roadmap({ background, roadmapData }) {
     return (
         <section id="roadmap" className={background}>
             <div className="padding-global">
                 <div className="container-large">
-                    <div className='margin-bottom-large'>
-                        <h5 className="heading-style-h7 is-lime">
-                            {roadmapData.head2}
-                        </h5>
+                    <div className="margin-bottom-large">
+                        <h5 className="heading-style-h7 is-lime">{roadmapData.head2}</h5>
                         <h3 className="heading-style-h3 margin-bottom-large white">
                             {roadmapData.head}
                         </h3>
                     </div>
-                    <Timeline mode="alternate">
-                                    {roadmapData.data.map((step, index) => {
-                                        return (
-                                            <Timeline.Item color={'#8366F4'} label={step.date}  className="roadmap-timeline">
-                                                <div id={index.toString()} className="background-color-off-green roadmap-item">
-                                                    <div>
-                                                        {step.goals.map((goal,index)=> {
+                    <Timeline mode={"alternate"}>
+                        {roadmapData.data.map((step, index) => {
+                            return (
+                                <Timeline.Item
+                                    color={"#8366F4"}
+                                    label={step.date}
+                                    className="roadmap-timeline"
+                                >
+                                    <div
+                                        id={index.toString()}
+                                        className="background-color-off-green roadmap-item"
+                                    >
+                                        <div>
+                                            {step.goals.map((goal, index) => {
+                                                return (
+                                                    <p className="roadmap-text" id={index.toString()}>
+                                                        {goal}
+                                                    </p>
+                                                );
+                                            })}
+                                        </div>
+                                        <div className={`${step.dates.length > 0 ? "" : "hide"}`}>
+                                            {step.dates.map((date, index) => {
+                                                return (
+                                                    <div id={index.toString()} className="roadmap-date">
+                                                        <h6 className="heading-style-h6">{date.date}</h6>
+                                                        {date.text.map((t, index) => {
                                                             return (
-                                                                <p  className="roadmap-text" id={index.toString()}>{goal}</p>
-                                                            )
+                                                                <p
+                                                                    className="roadmap-text"
+                                                                    id={index.toString()}
+                                                                >
+                                                                    {t}
+                                                                </p>
+                                                            );
                                                         })}
                                                     </div>
-                                                    <div className={`${step.dates.length > 0 ? '' : 'hide'}`}>
-                                                        {step.dates.map((date,index)=> {
-                                                            return (
-                                                                <div id={index.toString()} className="roadmap-date">
-                                                                    <h6 className="heading-style-h6">
-                                                                        {date.date}
-                                                                    </h6>
-                                                                    {date.text.map((t, index)=> {
-                                                                        return (
-                                                                            <p className="roadmap-text" id={index.toString()}>{t}</p>
-                                                                        )
-                                                                    })}
-                                                                </div>
-                                                            )
-                                                        })}
-                                                    </div>
-
-                                                </div>
-                                            </Timeline.Item>
-                                        )})}
+                                                );
+                                            })}
+                                        </div>
+                                    </div>
+                                </Timeline.Item>
+                            );
+                        })}
                     </Timeline>
                 </div>
             </div>
         </section>
-    )
+    );
 }
